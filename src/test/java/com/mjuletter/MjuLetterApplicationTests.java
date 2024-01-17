ea98c73ba46b855a5a1e62362097de539a23d33f
EunbeenDev <ros4312@naver.com>
2024-01-17 09:53:56 +0900

    feat: init commit

---
+++ src/test/java/com/mjuletter/MjuLetterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjuletter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MjuLetterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
