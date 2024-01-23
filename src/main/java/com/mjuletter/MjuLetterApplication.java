63b4c197ce4bcf2d23b06dd7985777d016a91281
EunbeenDev <ros4312@naver.com>
2024-01-24 03:42:57 +0900

    feat: init commit

--- src/main/java/com/mjuletter/MjuLetterApplication.java
+++ src/main/java/com/mjuletter/MjuLetterApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MjuLetterApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MjuLetterApplication.class, args);
     }
-
 }
