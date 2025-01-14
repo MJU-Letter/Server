cc37965390b71f0e862474220c5da0dd68c78e1e
jisu <98259433+jisu-jeong0@users.noreply.github.com>
2025-01-15 08:04:31 +0900

    [REFACTOR] 배포 방식 변경에 따른 코드 수정

--- src/main/java/com/mjuletter/MjuLetterApplication.java
+++ src/main/java/com/mjuletter/MjuLetterApplication.java
@@ -3,19 +3,16 @@ package com.mjuletter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import io.github.cdimascio.dotenv.Dotenv;
 
-@EnableJpaAuditing // Jpa Auditing 기능(생성일, 수정일 자동 생성 기능)을 사용할 수 있도록 추가
+import java.util.TimeZone;
+
+@EnableJpaAuditing
 @SpringBootApplication
 public class MjuLetterApplication {
 
     public static void main(String[] args) {
-        Dotenv dotenv = Dotenv.load();
-        System.setProperty("db.host", dotenv.get("DB_HOST"));
-        System.setProperty("db.name", dotenv.get("DB_NAME"));
-        System.setProperty("db.port", dotenv.get("DB_PORT"));
-        System.setProperty("db.user", dotenv.get("DB_USERNAME"));
-        System.setProperty("db.password", dotenv.get("DB_PASSWORD"));
+        // timezone 설정
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(MjuLetterApplication.class, args);
     }
 
